9f21b1a5d7c577c421de8c1a72b0223b84f7a422
zhuxiaotianya <3438235205@qq.com>
2025-04-26 21:40:18 +0800

    system_IO_display

---
+++ basic_IO/system_IO/system_IO_display.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+int main()
+{
+    char buf[1024];
+    ssize_t s = read(0, buf, sizeof(buf));
+    if (s > 0)
+    {
+        buf[s] = 0;
+        write(1, buf, strlen(buf));
+        write(2, buf, strlen(buf));
+    }
+    return 0;
+}
