fffc5a4d842fa103de49595879b108fcf7022b60
zhuxiaotianya <1119876462@qq.com>
2025-05-01 01:21:03 +0800

    alarm

---
+++ Signal/alarm/alarm.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    int count = 1;
+    alarm(1);
+    for (; 1;count++)
+    {
+        printf("count = %d\n", count);
+    }
+    return 0;
+}
