223d4435769c22884e8ec821a19b7396ddbe4b5d
zhuxiaotianya <1119876462@qq.com>
2025-05-01 16:17:03 +0800

    catch_SIGSEGV

---
+++ Signal/catch_SIGSEGV/catch_SIGSEGV.c
@@ -0,0 +1,19 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void handler(int sig)
+{
+    printf("catch a sig : %d\n", sig);
+}
+
+int main()
+{
+    signal(SIGSEGV, handler); // 信号是可以被自定义捕捉的,signal函数就是用来进行信号捕捉的,提前了解一下
+    sleep(1);
+    int *p = NULL;
+    *p = 100;
+    while (1)
+        ;
+    return 0;
+}
